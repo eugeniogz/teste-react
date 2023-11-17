efa3d917851d0574c06cbaae067adf610347a8ab
eugeniogz <eugeniog@gmail.com>
2023-11-16 21:14:30 -0300

    botão ok

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  var i=0;
+  i=i+1;
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={this.press} title="Ok">Ok</Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function press() {
+  var i=0;
+  i=i+1;
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
