1e2c567db2c8f662120826112aa015f49497e579
José Eugènio de Assis Gonçalves <eugeniog@gmail.com>
2023-11-19 19:53:43 -0300

    FAB ainda não funcionando

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { Suspense, Component, useState, useEffect } from 'react';
-import { Button, StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import React, { Suspense, Component, useState, useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { FloatingActionButton } from 'react-native-floating-action';
+import { Button, StyleSheet, Text, View, ActivityIndicator, ScrollView } from 'react-native';
+//import { FloatingActionButton } from 'react-native-elements'
 
 /*
 export default function App() {
@@ -46,19 +49,66 @@ const App = () => {
       );
     }
   
+    var data1 = [];
+    var data2 = [];
+    for (var i=0; i<data.length; i++) {
+      if (i % 2 == 0) 
+        data1.push(data[i]);
+      else
+        data2.push(data[i]);
+    }
     return (
-      <View style={styles.container}>
-        {data.map((memo) => <Text key={memo.id}>{memo.content}</Text>)}
-      </View>
+      <SafeAreaView style={styles.container}>
+        <ScrollView>
+          <View style={styles.view}>
+          <View style={styles.viewInterno}>
+        {data1.map((memo) => <Text onPress={() => pressionado()} style={styles.memo} key={memo.id}>{memo.content}</Text>)}
+          </View>
+          <View style={styles.viewInterno}>
+        {data2.map((memo) => <Text onPress={() => pressionado()} style={styles.memo} key={memo.id}>{memo.content}</Text>)}
+          </View>
+          </View>
+          <FloatingActionButton
+              icon={{ name: 'add' }}
+              onPress={()=> addItem()}
+              style={{
+                position: 'absolute',
+                right: 20,
+                bottom: 20,
+              }}
+          ></FloatingActionButton>
+        </ScrollView>
+      </SafeAreaView>
     );
   };
+
+  const addItem = () => {
+    //setItems([...items, { id: items.length + 1, name: `Item ${items.length + 1}` }]);
+  };
+ 
+  function pressionado() {
+    console.log("Logando");
+  }
   
   const styles = StyleSheet.create({
+    memo: {
+      // flex: 0.4,
+      padding: 4,
+      margin: 3,
+      backgroundColor: "#c0c0c0",
+    },
+    view: {
+      flex: 1, flexDirection: 'row',
+      // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 ,
+    },
+    viewInterno: {
+      flex: 0.5,
+      // paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0 ,
+    },
     container: {
       flex: 1,
-      backgroundColor: "#c0c0c0",
-      justifyContent: "center",
-      alignItems: "center",
+      // justifyContent: "center",
+      // alignItems: "center",
     },
   });
   
